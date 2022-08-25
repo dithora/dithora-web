e69f4b374b8f8fef55ab65f4880adb0d44bf0150
Dark Soul <71457016+darksoul228@users.noreply.github.com>
2022-08-25 15:12:34 +0500

    Create app.js

---
+++ src/js/app.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "././css/app.css";
+import { useTranslation } from "react-i18next";
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+
+  return (
+    <div className="#BUTTONS_LINK">
+      <button onClick={() => changeLanguage("en")}>EN</button>
+      <button onClick={() => changeLanguage("ru")}>RU</button>
+      <button onClick={() => changeLanguage("ua")}>UA</button>  
+  <hr />
+      <div><h1>{t("title")}</h1></div>
+      <div>{t("description.part1")}</div>
+      <div>{t("description.part2")}</div>
+    </div>
+  );
+}
+
+//Code taken from: https://github.com/mohnatus/i18next-template/
