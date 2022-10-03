90d075736088039214761dc1a28c0ebf936bd355
Dark Soul <71457016+darksoul228@users.noreply.github.com>
2022-10-03 21:52:03 +0500

    Create Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://dark-soul.space/license'
+      className='link footer__link'
+    >
+     license 
+    </a>
+  </footer>
+)
+
+export default Footer
