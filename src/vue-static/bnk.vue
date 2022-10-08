e0bf5a740d3a3b81af9712e8f186e3463eb43f10
Dark Soul <71457016+darksoul228@users.noreply.github.com>
2022-10-08 09:15:15 +0500

    Update bnk.vue

--- src/vue-static/bnk.vue
+++ src/vue-static/bnk.vue
@@ -1,2 +1,4 @@
 You are not allowed to access this resource.
 If you think an error has occurred, please contact email: darksoul.orgm@gmail.com
+
+<!-- Bad code is not bad at all. He just didn't get it right. --> 
