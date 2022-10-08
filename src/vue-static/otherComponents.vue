20895486dc69dfe319e3dc27ff5640e55934daf3
Dark Soul <71457016+darksoul228@users.noreply.github.com>
2022-10-08 09:16:28 +0500

    Update otherComponents.vue

--- src/vue-static/otherComponents.vue
+++ src/vue-static/otherComponents.vue
@@ -1,2 +1,4 @@
 You are not allowed to access this resource.
 If you think an error has occurred, please contact email: darksoul.orgm@gmail.com
+
+<!-- Before deleting files, make sure they are not yours. -->
