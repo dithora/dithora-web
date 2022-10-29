d7ddedc150d8cfb581b978ece7e30baa7fbcd240
Dark Soul <71457016+darksoul228@users.noreply.github.com>
2022-10-29 19:20:20 +0500

    Delete src/vue-static directory

--- src/vue-static/bnk.vue
+++
@@ -1,4 +0,0 @@
-You are not allowed to access this resource.
-If you think an error has occurred, please contact email: darksoul.orgm@gmail.com
-
-<!-- Bad code is not bad at all. He just didn't get it right. --> 
--- src/vue-static/otherComponents.vue
+++
@@ -1,4 +0,0 @@
-You are not allowed to access this resource.
-If you think an error has occurred, please contact email: darksoul.orgm@gmail.com
-
-<!-- Before deleting files, make sure they are not yours. -->
