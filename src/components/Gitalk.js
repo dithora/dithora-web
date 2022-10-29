4a072f635a850fe7b753d8f36afaede0f08c36fb
Dark Soul <71457016+darksoul228@users.noreply.github.com>
2022-10-29 20:56:40 +0500

    Create Gitalk.js

---
+++ src/components/Gitalk.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import 'gitalk/dist/gitalk.css'
+import GT from 'gitalk'
+import styled from 'styled-components'
+
+const DevWrapper = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  padding: 1rem;
+  border: 1px solid #ddd;
+`
+
+export default function Gitalk() {
+  const [isProd] = useState(process.env.NODE_ENV == 'production')
+  // const [isProd] = useState(process.env.NODE_ENV == 'development');
+  useEffect(() => {
+    if (isProd) {
+      const language =
+        navigator.language === 'en-EN' ? navigator.language : 'en'
+      const gitalk = new GT({
+        clientID: '',
+        clientSecret: '',
+        repo: 'yangerxiao.com',
+        owner: 'zerosoul',
+        admin: ['zerosoul'],
+        language,
+        id: location.pathname, // Ensure uniqueness and length less than 50
+        distractionFreeMode: false, // Facebook-like distraction free mode
+      })
+      gitalk.render('gitalk-container')
+    }
+  }, [isProd])
+  return isProd ? (
+    <div id="gitalk-container" />
+  ) : (
+    <DevWrapper>The development environment does not load the comment component</DevWrapper>
+  )
+}
