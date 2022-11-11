3564174f6bc84744227a5d77b2e14991efc18725
Dark Soul <71457016+darksoul228@users.noreply.github.com>
2022-11-11 20:02:33 +0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Page from "./components/page";
+
+
+ReactDOM.render(<Page />, document.getElementById("root"));
