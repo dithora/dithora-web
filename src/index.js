52c0c939a98432d84b8312adf271c454bb7d192b
Dark Soul <71457016+darksoul228@users.noreply.github.com>
2022-11-11 20:04:08 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+// imports, ../package.json 
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Page from "./components/page";
 
-
+// page render
 ReactDOM.render(<Page />, document.getElementById("root"));
