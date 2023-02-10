a1facb938599cde95bf3be0ba39d6c9a2ad05d1c
Dark Soul <darksouldf@users.noreply.github.com>
2023-02-10 17:56:14 +0500

    -

--- js/buildManifest.js
+++
@@ -1,7 +0,0 @@
-(self.__BUILD_MANIFEST = (function (e, s) {
-  return {
-    __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
-    "/": [e, s, "js/index-a87ae8a15f2bd65b.js"]
-  };
-})("css/b9e5e4c5ef22abcb.css", "js/509-2dbf5e9f2c227c39.js")),
-  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
